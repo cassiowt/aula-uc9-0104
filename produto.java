66d82d12d47dc0dddddcca98772ee90c60910534
Cássio Trindade <cassiowt@gmail.com>
2024-04-01 11:37:13 -0300

    Adicionado primeiro arquivo ao Commite

---
+++ produto.java
@@ -0,0 +1,15 @@
+package com.meuprojeto
+
+public produto {
+	private String name;	
+	private int preco;
+
+	public String getName() {
+	  return this.name;	
+	}
+
+	public setName(String name){
+	   this.name = name;
+	}
+
+}
